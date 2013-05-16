ffd21e1d9f95c49c83b217ba2bc8d7e0b308623e
Ben Ng <omnomnomben@gmail.com>
2013-05-15 22:40:48 -0700

    Init

---
+++ config/environment.js
@@ -0,0 +1,21 @@
+var config = {
+  /*
+  * Maps flash message keys to css classes
+  */
+  flashes: {
+    defaultClass: 'alert',
+    classes: {
+      success: 'alert alert-success',
+      alert: 'alert',
+      error: 'alert alert-error',
+      info: 'alert alert-info'
+    }
+  }
+  /*
+  metrics: {
+    port: 4001
+  }
+  */
+};
+
+module.exports = config;
